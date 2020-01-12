17954e2ef69c0547f0762e10d72f9c4f0cd35bf9
Kinga Giza-Rzeszot <kinga.giza.10@gmail.com>
2020-01-13 00:06:29 +0100

    Refactor Intro View

--- Adopt/Welcome/Welcome.IntroView.swift
+++ Adopt/Welcome/Welcome.IntroView.swift
@@ -9,53 +9,74 @@ import AVFoundation
 import SwiftUI
 
 extension Welcome {
+
     struct IntroView: View {
         var action: (Modal) -> Void
 
         var body: some View {
             VStack(spacing: 10) {
                 Spacer()
-                
-                HStack(alignment: .center, spacing: 5) {
-                    Image("paw")
-                        .foregroundColor(Color(#colorLiteral(red: 0.9058823529, green: 0.08235294118, blue: 0.3803921569, alpha: 1)))
-                
-                    Text("Adopt!")
-                        .font(Font.custom("offside-regular", size: 40))
-                }
+
+                LogoView()
 
                 Spacer()
-
-                Button(action: {
+                
+                ActionButton(action: {
                     self.action(.login)
-                }, label: {
-                    HStack {
-                        Spacer()
-                        Text("Log In to Existing Account")
-                            .font(Font.custom("Montserrat-SemiBold", size: 16))
-                            .foregroundColor(Color(.systemBackground))
-                        Spacer()
-                    }
-                    .padding(20)
-                    .background(Color(#colorLiteral(red: 0.9058823529, green: 0.08235294118, blue: 0.3803921569, alpha: 1)))
-                    .cornerRadius(10)
-                })
-
-                Button(action: {
+                }, text: {
+                    Text("Log In to Existing Account")
+                        .font(Font.custom("Montserrat-SemiBold", size: 16))
+                        .foregroundColor(Color(.systemBackground))
+                }, color: Color(#colorLiteral(red: 0.9058823529, green: 0.08235294118, blue: 0.3803921569, alpha: 1)))
+                
+                
+                ActionButton(action: {
                     self.action(.register)
-                }, label: {
-                    HStack {
-                        Spacer()
-                        Text("Create New Account")
-                            .foregroundColor(Color(#colorLiteral(red: 0.9058823529, green: 0.08235294118, blue: 0.3803921569, alpha: 1)))
-                        Spacer()
-                    }
-                    .padding(20)
+                }, text: {
+                    Text("Create New Account")
+                        .foregroundColor(Color(#colorLiteral(red: 0.9058823529, green: 0.08235294118, blue: 0.3803921569, alpha: 1)))
                 })
             }
             .padding(.horizontal, 20)
         }
     }
+    
+    struct ActionButton: View {
+        var action: () -> Void
+        var text: () -> Text
+        var color: Color? = nil
+
+        var body: some View {
+            Button(action: action, label: {
+                HStack {
+                    Spacer()
+                    text()
+                    Spacer()
+                }
+                .padding(20)
+                .background(color ?? .clear)
+                .cornerRadius(10)
+            })
+        }
+    }
+    
+    struct LogoView: View {
+        var body: some View {
+            HStack(alignment: .center, spacing: 0) {
+                Image("paw")
+                    .foregroundColor(Color(#colorLiteral(red: 0.9058823529, green: 0.08235294118, blue: 0.3803921569, alpha: 1)))
+                    .offset(x: -8, y: 0)
+
+                Text("Adopt")
+                    .font(Font.custom("offside-regular", size: 40))
+
+                Text("!")
+                    .font(Font.custom("offside-regular", size: 40))
+                    .foregroundColor(Color(#colorLiteral(red: 0.9058823529, green: 0.08235294118, blue: 0.3803921569, alpha: 1)))
+            }
+        }
+    }
+
 }
 
 struct Welcome_IntroView_Previews: PreviewProvider {
