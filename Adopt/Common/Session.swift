8b5c2babcc3c231ba7305d7634fa5b26085fa453
Damian Rzeszot <damian.rzeszot@gmail.com>
2020-03-22 18:48:14 +0100

    Preserve session

--- Adopt/Common/Session.swift
+++ Adopt/Common/Session.swift
@@ -1,4 +1,5 @@
 //
+//
 //  Copyright © 2019 Damian Rzeszot. All rights reserved.
 //
 
@@ -6,25 +7,51 @@ import Foundation
 
 class Session {
 
-    struct Credentials {
+    class Credentials: Codable {
         let email: String
         let token: String
+
+        init(email: String, token: String) {
+            self.email = email
+            self.token = token
+        }
     }
 
     private(set) var credentials: Credentials?
 
-    init(credentials: Credentials? = nil) {
-        self.credentials = credentials
-    }
+    let defaults: UserDefaults
 
-    // MARK: -
+    init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+        self.credentials = defaults.credentials(for: "auth")
+    }
 
     func login(_ credentials: Credentials) {
         self.credentials = credentials
+        self.defaults.set(credentials, for: "auth")
     }
 
     func logout() {
         credentials = nil
+        defaults.set(nil, for: "auth")
+    }
+
+}
+
+private extension UserDefaults {
+
+    func set(_ credentials: Session.Credentials?, for key: String) {
+        if let credentials = credentials {
+            let value = try? JSONEncoder().encode(credentials)
+            set(value, forKey: key)
+        } else {
+            removeObject(forKey: key)
+        }
+    }
+
+    func credentials(for key: String) -> Session.Credentials? {
+        guard let value = data(forKey: key) else { return nil }
+        return try? JSONDecoder().decode(Session.Credentials.self, from: value)
     }
 
 }
