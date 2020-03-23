c099b06ad2a38ee5f25ad3597de1e81cd6bbcd4d
Damian Rzeszot <damian.rzeszot@gmail.com>
2020-03-23 08:03:16 +0100

    Unit tests

---
+++ UnitTests/ExampleTests.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2020 Damian Rzeszot. All rights reserved.
+//
+
+import XCTest
+
+class ExampleTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+
+}
