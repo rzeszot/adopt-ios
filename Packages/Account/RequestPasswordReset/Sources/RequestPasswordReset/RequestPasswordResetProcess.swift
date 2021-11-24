4afd93e392a0c3b73935bd201b3328adbf598b94
Damian Rzeszot <damian.rzeszot@gmail.com>
2021-11-24 02:43:08 +0100

    Test

--- Packages/Account/RequestPasswordReset/Sources/RequestPasswordReset/RequestPasswordResetProcess.swift
+++ Packages/Account/RequestPasswordReset/Sources/RequestPasswordReset/RequestPasswordResetProcess.swift
@@ -1,5 +1,34 @@
 import Foundation
 
+
+struct CloseStep {
+  let reason: Input.CloseReason
+}
+
+
+struct EnterUsernameStep {
+  func close() -> CloseStep {
+    CloseStep(reason: .cancel)
+  }
+
+  func submit(username: String) -> EmailSentStep {
+    EmailSentStep()
+  }
+}
+
+
+struct EmailSentStep {
+  func back() -> EnterUsernameStep {
+    EnterUsernameStep()
+  }
+
+  func done() -> CloseStep {
+    CloseStep(reason: .done)
+  }
+}
+
+
+
 class RequestPasswordResetProcess {
 
 }
